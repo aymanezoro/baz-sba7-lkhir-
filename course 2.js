f41bac954e50ce3280c13a9dd0890286e793df84
aymanezoro <155267547+aymanezoro@users.noreply.github.com>
2023-12-30 22:54:45 +0100

    Add files via upload

---
+++ course 2.js
@@ -0,0 +1,17 @@
+
+
+function sound(){
+    var snd = new Audio('sba7 lkir.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
+
+
+
+
+
+
+
+
+//sba7 lkir.mp3
+// //<source src="sba7 lkir.mp3" type="audio/mp3"
